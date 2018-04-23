55d360e65b9e117813f64b72ff4f5a52042ed752
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-04-23 10:01:47 -0600

    Update slides script

--- index.md
+++ index.md
@@ -6,20 +6,36 @@ date: April 20th, 2018
 
 # Title 1 {slide-level=2}
 
-## Subsection
+Subtext
+
+## Subsection 1
+
+- Bullet points
+- Can use Markdown
+
+# Title 3
+
+## Subsection 3
+
+- Bullet points
+- Can use Markdown
+
+## Subsection 3
 
 - Bullet points
 - Can use Markdown
 
 # Title 2 {slide-level=2}
 
-## Subsection 1
-
-- Point1
-- Point2
-
 ## Subsection 2
 
-- Point1
-- Point2
+- Bullet points
+- Can use Markdown
+
+# Title 3
+
+## Subsection 3
+
+- Bullet points
+- Can use Markdown
 
--- scripts/fix-headers.py
+++ scripts/fix-headers.py
@@ -7,17 +7,76 @@ import panflute as pf
 
 ignore_elements = []
 
+def stringify(*args):
+    for arg in args:
+        if isinstance(arg, pf.Space):
+            continue
+        else:
+            yield arg.text
+
+def titlelify(content):
+    return "-".join([x.lower() for x in stringify(*content)])
+
+def print(*args):
+    for arg in args:
+        sys.stderr.write("{}\n".format(str(arg)))
+        sys.stderr.flush()
 
 def fix_header_level(elem, doc):
-    sys.stderr.write(str(elem))
-    pass
+    preblock = []
+    if isinstance(elem, pf.Header) and elem.level == 1:
+        if doc.is_in_section is True:
+            doc.is_in_section = False
+            preblock.append(pf.RawBlock("</section>"))
+        if 'slide-level' in elem.attributes:
+            if doc.is_in_subsection is True:
+                doc.is_in_subsection = False
+                preblock.append(pf.RawBlock("</section>"))
+            if doc.is_first_slide is False:
+                preblock.append(pf.RawBlock("""<section id={} class="slide level1">""".format(titlelify(elem.content))))
 
+            doc.is_in_section = True
+            doc.is_first_slide = False
+            content = elem.content
+            elem.content = []
+            doc.is_changed = True
+            return [
+                *preblock,
+                pf.RawBlock("<section>"),
+                pf.Header(*content, level=2), # TODO: convert to H1?
+            ]
+        else:
+            doc.is_changed = False
+            return [
+                *preblock,
+                elem,
+            ]
+
+    if isinstance(elem, pf.Header) and elem.level == 2:
+        if doc.is_in_section is True:
+            preblock.append(pf.RawBlock("</section>"))
+        if doc.is_in_section is True:
+            doc.is_in_subsection = True
+            content = elem.content
+            elem.content = []
+            return [
+                *preblock,
+                pf.RawBlock("<section>"),
+                elem
+            ]
+
+def prepare(doc):
+    doc.is_changed = False
+    doc.is_in_section = False
+    doc.is_in_subsection = False
+    doc.is_first_slide = True
 
 def finalize(doc):
-    pass
+    if doc.is_changed is True:
+        doc.content.append(pf.RawBlock("</section>"))
 
 def main(doc=None):
-    return pf.run_filter(fix_header_level, doc=doc, finalize=finalize)
+    return pf.run_filter(fix_header_level, prepare=prepare, doc=doc, finalize=finalize)
 
 
 if __name__ == "__main__":
